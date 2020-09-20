b08906c13f8bcc3bcc925a0b2c027e6eef04ac8f
podvistorcheto <denev.s.deyan@gmail.com>
2020-09-20 13:02:16 +0000

    01 - API added to todos_list app

---
+++ api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics, permissions
+from .serializer import TodoSerializer
+from todo.models import Todo
+
+
+class TodoCompletedList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
+        
